15642c532e90b7b549d5842f276b29c7da9e23e5
Pavel Novik <brn2befree@gmail.com>
2023-08-25 15:54:44 +0300

    Create learning project

---
+++ index.js
@@ -0,0 +1,36 @@
+// Array.prototype.last = function () {
+//   return this.length > 0 ? this.pop() : -1;
+// };
+
+// const arr = [];
+// console.log(arr.last());
+/**
+ */
+
+/**
+//  * @param {number} n
+//  * @return {Function} counter
+ **/
+// const createCounter = function (n) {
+//   let res = n;
+
+//   return function () {
+//     return n++;
+//   };
+// };
+
+// const counter = createCounter(10);
+// console.log(counter()); // 10
+// console.log(counter()); // 11
+// console.log(counter()); // 12
+// console.log(counter()); // 12
+// console.log(counter()); // 12
+// console.log(counter()); // 12
+
+// const nextCounter = createCounter(25);
+// console.log(nextCounter());
+// console.log(nextCounter());
+// console.log(nextCounter());
+// console.log(nextCounter());
+// console.log(nextCounter());
+// console.log(nextCounter());
